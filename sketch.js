f4a4df48ba172014034827d301e5ddb003f66ac2
kazimsanlav <kazimsanlav@gmail.com>
2018-08-26 12:38:54 +0300

    Add DOMs

--- sketch.js
+++ sketch.js
@@ -1,17 +1,44 @@
 // Getting video from camera then proccess it to put it on canvas
 
-var video;
-var vidScale = 4;
+var video, canvas;
+var vidScale = 16;
+var b_w = true;
 
 
 function setup() {
-	createCanvas(640, 480);
-	// background(255);
+	canvas = createCanvas(windowWidth, windowHeight - 20);
+	background(0);
 	pixelDensity(1);
 	video = createCapture(VIDEO);
 	video.size(width / vidScale, height / vidScale);
-	// video.hide();
+	video.hide();
 	ellipseMode(CENTER);
+
+	//capture button
+	var btn = createButton('Capture');
+	btn.position(windowWidth / 2 - 15, windowHeight - 35);
+	btn.mouseClicked(function () {
+		save();
+	});
+
+	//slider for vidScale
+	var scaleSlider = createSlider(8, 32, 16, 8);
+	scaleSlider.position(windowWidth / 2 - 250, windowHeight - 35);
+	scaleSlider.changed(function () {
+		vidScale = this.value();
+		video = createCapture(VIDEO);
+		video.size(width / vidScale, height / vidScale);
+		video.hide();
+	})
+
+	//create checkbox for color
+	var checkcolor = createCheckbox('Color');
+	checkcolor.position(windowWidth / 2 + 200, windowHeight - 35);
+	checkcolor.style('color', '#fff');
+	checkcolor.changed(function () {
+		b_w = !b_w;
+	})
+
 }
 
 function draw() {
@@ -30,7 +57,11 @@ function draw() {
 			var b = video.pixels[index + 2];
 			var bright = (r + g + b) / 3;
 
-			fill(bright);
+			if (b_w) {
+				fill(bright);
+			} else {
+				fill(r, g, b);
+			}
 			noStroke();
 			var rad = map(bright, 0, 255, 0, vidScale);
 			ellipse(x * vidScale, y * vidScale, rad);
